698376253777c43c1666addd8b2dde90b5711556
sarakikun <jaden23sun@gmail.com>
2021-01-03 20:48:23 -0500

    updated 1/03/21 5:48 PM

--- bot.py
+++ bot.py
@@ -1,3 +1,17 @@
 import sys
 import discord
-import
+import math
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+client.run(token)
