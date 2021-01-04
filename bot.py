359a858fe649e59cf83c1577bc0e9b9e79e8551c
sarakikun <jaden23sun@gmail.com>
2021-01-04 17:13:17 -0500

    updated 1/04/21 2:13 PST

--- bot.py
+++ bot.py
@@ -3,15 +3,21 @@ import discord
 import math
 import os
 
+from discord.ext import commands
 from dotenv import load_dotenv
 
-load_dotenv()
-token = os.getenv('DISCORD_TOKEN')
-
-client = discord.Client()
+client = commands.Bot(command_prefix = '!')
 
 @client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+async def on_ready(self):
+    print("Bot is of operation")
 
-client.run(token)
+@client.event
+async def on_member_join(member):
+    print(f'(member) has joined the current server')
+
+@client.event
+async def on_member_leave(member):
+    print(f'(member) has left the current server')
+
+client.run('Nzk0MDM5NTk2OTQxMTgwOTY5.X-1BAA.IWAS7N_kHo_NOQjuGEi7I9hrh3E')
