77fefdbe1c273f0d80aa491c23c1aa5f591e812a
sarakikun <jaden23sun@gmail.com>
2021-01-06 18:02:04 -0500

    updated with code errors 01/06/21 3:01 PM PST

--- bot.py
+++ bot.py
@@ -5,44 +5,48 @@ import os
 import random
 
 from discord.ext import commands
-from dotenv import load_dotenv
 
 default_prefix = ['!']
 client = commands.Bot(command_prefix = default_prefix)
 
 
 @client.event
-async def on_ready(ctx):
+async def on_ready():
     print("Bot is of operation")
-    await client.get_channel(793847966162485288).send("Bot is online!")
+    await client.get_channel(795796608461176845).send("Bot is online!")
 
 @client.event
 async def on_member_join(member):
     print(f'(member) has joined the current server')
+    await client.get_channel(795796608461176845).send(f'(member) has joined the server I guess')
 
 @client.event
 async def on_member_leave(member):
     print(f'(member) has left the current server')
+    await client.get_channel(795796608461176845).send(f'(member) left the server, oh well')
 
 @client.command()
 async def ping(ctx):
-    if {round(client.latency * 1000)} > 20: {
-        await ctx.send(f'ping is {round(client.latency * 1000)}ms, you got cheap walmart connection?')
-    else:
-        await ctx.send(f'ping is {round(client.latency * 1000)}ms')
+    await ctx.send(f'ping is {round(client.latency * 1000)}ms loser')
+
 
 @client.command(pass_context = True)
 async def join(ctx):
-    channel = ctx.message.author.voice.voice_channel
-    voice_client = ctx.member.voice
-
-    if voice_client is None:
-        return await ctx.send('Must be in voice channel to use command')
+    channel = ctx.message.author.voice.channel
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if channel == None:
+        await ctx.send("Must be in voice channel to use command")
+        return
+    if voice and voice.is_connected():
+        await voice.move_to(channel)
+        await client.get_channel(795796608461176845).send("I joined the channel!")
     else:
-        await client.join_voice_channel(channel)
+        voice = await channel.connect()
+        await client.get_channel(795796608461176845).send("I joined the channel!")
 
 @client.command()
 async def leave(ctx):
     await ctx.voice_client.disconnect()
+    await client.get_channel(795796608461176845).send("I left the channel, see ya'll next time")
 
 client.run('TOKEN HERE')
